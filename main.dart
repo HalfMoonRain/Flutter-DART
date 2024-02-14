0a8b4f6a2070fddeff3837ceab1365a311540d59
JunWoo Lee <junwoo1357@gmail.com>
2024-02-14 23:43:55 +0900

    Lists

--- main.dart
+++ main.dart
@@ -1,7 +1,20 @@
 void main() {
-  String name = "nico";
-  bool alive = true;
-  int age = 12;
-  double money = 68.66;
-  num x = 12;
+  var numbers = [1, 2, 3, 4];
+
+  numbers.add(1);
+
+  // collection if
+  var giveMeFive = true;
+
+  List<int> numberss = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+  // 이거랑 같은의미 더 간단하게 쓸 수 있다.
+  if (giveMeFive) {
+    numberss.add(5);
+  }
 }
