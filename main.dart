8545f0b70f4ccbc75dd695802f8fe9796f0866ec
JunWoo Lee <junwoo1357@gmail.com>
2024-02-15 00:04:57 +0900

    Maps

--- main.dart
+++ main.dart
@@ -1,9 +1,16 @@
 void main() {
-  var oldFriends = ['nico', 'pinkman'];
-  var newFriends = [
-    'WW',
-    'saul',
-    for (var friend in oldFriends) "BB $friend",
-  ];
-  print(newFriends);
+  var player = {
+    'name': 'nico',
+    'xp': 20.05,
+    'superpower': false,
+  };
+
+  Map<int, bool> numbers = {
+    1: true,
+    2: false,
+  };
+  Map<List<int>, bool> complex = {
+    [1, 2, 3]: true,
+    [2, 4, 6]: false,
+  };
 }
